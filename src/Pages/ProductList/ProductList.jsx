3b77602f0b68ea512ab00c377cb31cfe803b8312
habib16247 <habib162470@gmail.com>
2023-11-27 20:45:30 +0600

    Modified

--- src/Pages/ProductList/ProductList.jsx
+++ src/Pages/ProductList/ProductList.jsx
@@ -31,8 +31,8 @@ const ProductList = () => {
     const addHandler = (e) => {
         e.preventDefault();
         
-        if(!title || !productId || !price || !quantity || !description) {
-            return alert("g eiuhr8 ber8uhf ew ew8f dyhfw ")
+        if(!title || !productId || !price || !quantity || !description || !date) {
+            return alert("Enter Product Details!!!!")
         }
 
         const productInfo = {
@@ -52,7 +52,7 @@ const ProductList = () => {
         setQuantity("");
         setDescription("");
         setDate("")
-        setColors("red")
+        setColors("")
     }
 
     const deleteHandler = (id) => {
@@ -141,9 +141,3 @@ const ProductList = () => {
 }
 
 export default ProductList
-
-// - Create a FormData components, the form contains input fields like Product Name, Product Id, Price, Quantity, Description, a select menu that contains three colors as options like "red","blue","green".
-// - When submit the form data it should be stored in local-storage.
-// - There should be another component called ProductTable that contain the ProductRow component.
-// - ProductRow contains the info of products in a table row data.
-// - Integrate user interactions to add, delete and data should be stored in local-storage.
